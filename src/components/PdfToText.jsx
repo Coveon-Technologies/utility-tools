4c1a17bcb4d2c8508cde51da65565492a0a0e62f
Shaswat Paudel <shaswat@ezops.ca>
2023-02-16 13:35:23 -0700

    react-pdf

--- src/components/PdfToText.jsx
+++ src/components/PdfToText.jsx
@@ -1,14 +1,22 @@
 import React, { useState } from "react";
 import * as pdfjsLib from "pdfjs-dist/webpack";
+import { Document, Page, pdfjs } from 'react-pdf';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const PdfToText = () => {
+
   const [text, setText] = useState("");
+  const [pdfFile, setPdfFile] = useState(null);
+
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1)
 
   const extractTextFromPDF = async (pdfUrl) => {
     console.log(pdfUrl)
     const loadingTask = await pdfjsLib.getDocument(pdfUrl);
     const pdf = await loadingTask.promise;
     const numPages = pdf.numPages;
+    setNumPages(pageNumber);
 
     let pdfText = "";
     for (let i = 1; i <= numPages; i++) {
@@ -24,6 +32,7 @@ const PdfToText = () => {
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
+    setPdfFile(file)
     const fileReader = new FileReader();
     fileReader.onloadend = () => {
       extractTextFromPDF(fileReader.result);
@@ -35,6 +44,14 @@ const PdfToText = () => {
     <div>
       <input type="file" onChange={handleFileChange} />
       <div>{text}</div>
+      {pdfFile && (
+        <Document file={pdfFile}>
+          {Array.from(new Array(numPages), (el, index) => (
+            <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+          ))}
+        </Document>
+      )}
+
     </div>
   );
 };
