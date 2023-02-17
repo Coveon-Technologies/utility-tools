791b165e06ff8f1b315d5af1eeee165f678ff2ec
Shaswat Paudel <shaswat@ezops.ca>
2023-02-16 18:17:23 -0700

    Fixed

--- src/components/PdfToText.jsx
+++ src/components/PdfToText.jsx
@@ -1,56 +1,71 @@
-import React, { useState } from "react";
-import * as pdfjsLib from "pdfjs-dist/webpack";
-import { Document, Page, pdfjs } from 'react-pdf';
+import React, { useState, useRef } from 'react';
+import * as pdfjs from 'pdfjs-dist';
+import './tooltip.css'
+
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const PdfToText = () => {
+  const [pdfText, setPdfText] = useState('');
+  const [definition, setDefinition] = useState('');
 
-  const [text, setText] = useState("");
-  const [pdfFile, setPdfFile] = useState(null);
-
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1)
-
-  const extractTextFromPDF = async (pdfUrl) => {
-    console.log(pdfUrl)
-    const loadingTask = await pdfjsLib.getDocument(pdfUrl);
-    const pdf = await loadingTask.promise;
-    const numPages = pdf.numPages;
-    setNumPages(pageNumber);
-
-    let pdfText = "";
-    for (let i = 1; i <= numPages; i++) {
-      const page = await pdf.getPage(i);
-      const textContent = await page.getTextContent();
-      const pageText = textContent.items
-        .map((item) => item.str)
-        .join("\n");
-      pdfText += pageText;
-    }
-    setText(pdfText);
-  };
-
-  const handleFileChange = (event) => {
+  const handleFileSelect = (event) => {
     const file = event.target.files[0];
-    setPdfFile(file)
-    const fileReader = new FileReader();
-    fileReader.onloadend = () => {
-      extractTextFromPDF(fileReader.result);
+    const reader = new FileReader();
+
+    reader.onload = async (event) => {
+      const typedArray = new Uint8Array(event.target.result);
+      const pdf = await pdfjs.getDocument({ data: typedArray }).promise;
+      let text = '';
+      for (let i = 1; i <= pdf.numPages; i++) {
+        const page = await pdf.getPage(i);
+        const textContent = await page.getTextContent();
+        const pageText = textContent.items.map(item => item.str).join(' ');
+        text += pageText + ' ';
+      }
+      setPdfText(text);
+      setDefinition('Fake');
     };
-    fileReader.readAsArrayBuffer(file);
+
+    reader.readAsArrayBuffer(file);
   };
+
+
+  const handleTooltip = (event) => {
+    const selection = window.getSelection();
+    const selectedWord = selection.toString().trim();
+    console.log(selectedWord)
+
+    if (selectedWord !== '') {
+      const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${selectedWord}`;
+
+      fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+          if (data && data.length > 0 && data[0].meanings[0]) {
+            const def = data[0].meanings[0].definitions[0].definition;
+            console.log(data[0])
+            setDefinition(def);
+            const tooltip = document.querySelector('.tooltip');
+            tooltip.style.display = 'block';
+            tooltip.style.top = `${event.pageY}px`;
+            tooltip.style.left = `${event.pageX}px`;
+            console.log(`Tooltip position: top=${tooltip.style.top}, left=${tooltip.style.left}`);
+          }
+        })
+        .catch(error => console.log(error));
+    } else {
+      setDefinition('');
+      const tooltip = document.querySelector('.tooltip');
+      tooltip.style.display = 'none';
+    }
+  };
+
 
   return (
     <div>
-      <input type="file" onChange={handleFileChange} />
-      <div>{text}</div>
-      {pdfFile && (
-        <Document file={pdfFile}>
-          {Array.from(new Array(numPages), (el, index) => (
-            <Page key={`page_${index + 1}`} pageNumber={index + 1} />
-          ))}
-        </Document>
-      )}
+      <input type="file" onChange={handleFileSelect} />
+      <div className="pdf-text" onMouseUp={handleTooltip} dangerouslySetInnerHTML={{ __html: pdfText.replace(/(consideration)/gi, '<span>$1</span>') }}></div>
+      <div className="tooltip">{definition}</div>
 
     </div>
   );
